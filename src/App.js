b201cad521cb68da7544aef2ce909889b2b677cb
marviecodes <marvnonso554@gmail.com>
2024-04-18 09:50:37 +0100

    Finished body layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import Exchanges from "./components/Exchanges";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import CryptoDetails from "./components/CryptoDetails";
+import News from "./components/News";
+import "./App.css";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+
+              {/* <Route exact path="/exchanges">
+                <Exchanges />
+              </Route>
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path="/crypto/:coinId">
+                <CryptoDetails />
+              </Route>
+              <Route exact path="/news">
+                <News />
+              </Route> */}
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+        <Typography.Title>
+          CryptoWorld <br />
+          All rights reserved
+        </Typography.Title>
+      </div>
     </div>
   );
 };
